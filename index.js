9755abc87e9417cdba59fac74ed9fb19d7a3f4c0
hayashikentarou <jacques.n>
2023-03-29 17:33:03 +0900

    remove parcel for get error on library install.

---
+++ index.js
@@ -0,0 +1,67 @@
+(() => {
+  // output/Data.Show/foreign.js
+  var showIntImpl = function(n) {
+    return n.toString();
+  };
+  var showStringImpl = function(s) {
+    var l = s.length;
+    return '"' + s.replace(
+      /[\0-\x1F\x7F"\\]/g,
+      // eslint-disable-line no-control-regex
+      function(c, i) {
+        switch (c) {
+          case '"':
+          case "\\":
+            return "\\" + c;
+          case "\x07":
+            return "\\a";
+          case "\b":
+            return "\\b";
+          case "\f":
+            return "\\f";
+          case "\n":
+            return "\\n";
+          case "\r":
+            return "\\r";
+          case "	":
+            return "\\t";
+          case "\v":
+            return "\\v";
+        }
+        var k = i + 1;
+        var empty = k < l && s[k] >= "0" && s[k] <= "9" ? "\\&" : "";
+        return "\\" + c.charCodeAt(0).toString(10) + empty;
+      }
+    ) + '"';
+  };
+
+  // output/Data.Show/index.js
+  var showString = {
+    show: showStringImpl
+  };
+  var showInt = {
+    show: showIntImpl
+  };
+  var show = function(dict) {
+    return dict.show;
+  };
+
+  // output/Effect.Console/foreign.js
+  var log = function(s) {
+    return function() {
+      console.log(s);
+    };
+  };
+
+  // output/Main/index.js
+  var calc = function(a) {
+    return a + 1 | 0;
+  };
+  var appText = /* @__PURE__ */ show(showInt)(/* @__PURE__ */ calc(2));
+  var main = /* @__PURE__ */ function() {
+    return log("The answer is " + show(showString)(appText));
+  }();
+
+  // <stdin>
+  main();
+})();
--- src/Main.purs
+++ src/Main.purs
@@ -4,9 +4,13 @@ import Prelude
 import Control.Plus (empty)
 import Data.List (List(..), filter, head)
 import Data.Maybe (Maybe)
+import Effect.Console (log)
 
 calc :: Int -> Int
 calc a = a + 1
 
 appText :: String
 appText = (show <<< calc) 2
+
+main = do
+  log ("The answer is " <> show appText)
